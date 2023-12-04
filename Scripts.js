db6eafeb6503634a8b6f3b03514c6ca8149a5cc2
GooseBooze <137728081+GooseBooze@users.noreply.github.com>
2023-12-04 14:49:19 +0100

    ss

--- Scripts.js
+++ Scripts.js
@@ -8,12 +8,7 @@ class NavComponent extends HTMLElement {
             <nav>
                 <ul>
                     <li><a href="index.html">Home Page</a></li>
-                    <li><a href="hobbies.html">My Hobbies</a></li>
-                    <li><a href="gallery.html">Gallery</a></li>
-                    <li><a href="video-gallery.html">Video Gallery</a></li>
-                    <li><a href="music.html">Music likes</a></li>
-                    <li><a href="awards.html">Awards</a></li>
-                    <li><a href="admin-protected.html">Admin</a></li>
+                    <li><a href="Page2.html">Favourite Games</a></li>
                 </ul>
             </nav>
         `;
