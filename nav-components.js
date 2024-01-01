ccecf3c91b30fae994a0bdd07a1f8481dd2125b9
GooseBooze <137728081+GooseBooze@users.noreply.github.com>
2024-01-02 00:58:30 +0100

    sss

--- nav-components.js
+++ nav-components.js
@@ -5,14 +5,14 @@ class NavComponent extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-            <nav-components>
+            <nav-component>
                 <nav>
                     <ul>
                         <li><a href="index.html">Home Page</a></li>
                         <li><a href="Page2.html">Game Library</a></li>
                     </ul>
                 </nav>
-            </nav-components>
+            </nav-component>
         `;
     }
 }
