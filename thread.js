c999c2f260ad68fa7cabd42704e7b22051b986d2
GooseBooze <137728081+GooseBooze@users.noreply.github.com>
2024-01-22 17:45:46 +0100

    sss

--- thread.js
+++ thread.js
@@ -1,19 +1,19 @@
-$(function(){
-    $("#form").submit(function(e){
-        // Create data object
-        var data = {};
-        data.name = $("#name").val();
-        data.email = $("#email").val();
-        data.comment = $("#comment").val();
-        // The url of your server-side script that handles the post submission
-        var url = "http://www.someurl.com/post_comment.php";
-        $.POST(url, data)
-        .done(function(response){
-            // This code executes when the server returns a response
-            // Do something with the response like adding the comment to the current list of comments
-            // Example (if your response is HTML, better would be a JSON string):
-            $("#comments").append(response);
-        });
-        e.preventDefault();
-    });
-});
+function addComment() {
+    var name = document.getElementById('name').value;
+    var comment = document.getElementById('comment').value;
+
+    if (name && comment) {
+        var commentSection = document.getElementById('comments');
+
+        var newComment = document.createElement('div');
+        newComment.innerHTML = '<strong>' + name + ':</strong> ' + comment;
+
+        commentSection.appendChild(newComment);
+
+        // Clear the input fields
+        document.getElementById('name').value = '';
+        document.getElementById('comment').value = '';
+    } else {
+        alert('Please enter both your name and a comment.');
+    }
+}
