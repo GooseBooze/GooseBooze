d84d27de421647fc97249014baab2a89780aa7ce
GooseBooze <137728081+GooseBooze@users.noreply.github.com>
2024-01-22 18:38:16 +0100

    ss

--- thread.js
+++ thread.js
@@ -1,3 +1,10 @@
+// In thread.js
+
+// ...
+
+// Initialize captcha on page load
+updateCaptcha();
+
 function submitComment() {
     var nameInput = document.getElementById('name');
     var commentInput = document.getElementById('comment');
@@ -28,57 +35,9 @@ function submitComment() {
     }
 }
 
-function displayComment(comment) {
-    var commentSection = document.getElementById('comments');
-
-    var newComment = document.createElement('div');
-    newComment.classList.add('comment');
-    newComment.innerHTML = '<strong>' + comment.name + ':</strong> ' + comment.comment;
-
-    commentSection.appendChild(newComment);
-}
-
-function clearInputFields(...inputs) {
-    inputs.forEach(input => (input.value = ''));
-}
-
-function isValidCaptcha(userAnswer) {
-    var correctAnswer = getCorrectCaptcha();
-    return !isNaN(userAnswer) && userAnswer === correctAnswer;
-}
-
-function getCorrectCaptcha() {
-    // Generate a simple addition problem for the captcha
-    var num1 = Math.floor(Math.random() * 10) + 1;
-    var num2 = Math.floor(Math.random() * 10) + 1;
-
-    // Display the captcha question in the form
-    document.getElementById('captcha-question').innerText = num1 + ' + ' + num2;
-
-    // Return the correct answer
-    return num1 + num2;
-}
+// ...
 
 function updateCaptcha() {
     // Regenerate the captcha for the next comment
     getCorrectCaptcha();
-}
-
-// Load existing comments from cookies when the page loads
-window.onload = function () {
-    // Your existing code for loading comments here
-};
-
-function displayComment(comment) {
-    var commentSection = document.getElementById('comments');
-
-    var newComment = document.createElement('div');
-    newComment.classList.add('comment');
-    newComment.innerHTML = '<strong>' + comment.name + ':</strong> ' + comment.comment;
-
-    commentSection.appendChild(newComment);
-}
-
-function clearInputFields(...inputs) {
-    inputs.forEach(input => (input.value = ''));
 }
