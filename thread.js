da6c52835d420af4f026beeb333e7d9f980e7947
GooseBooze <137728081+GooseBooze@users.noreply.github.com>
2024-01-22 17:59:01 +0100

    ss

--- thread.js
+++ thread.js
@@ -1,19 +1,35 @@
-function addComment() {
-    var name = document.getElementById('name').value;
-    var comment = document.getElementById('comment').value;
+function submitComment() {
+    var nameInput = document.getElementById('name');
+    var commentInput = document.getElementById('comment');
+
+    var name = nameInput.value.trim();
+    var comment = commentInput.value.trim();
 
     if (name && comment) {
         var commentSection = document.getElementById('comments');
 
-        var newComment = document.createElement('div');
-        newComment.innerHTML = '<strong>' + name + ':</strong> ' + comment;
-
+        var newComment = createCommentElement(name, comment);
         commentSection.appendChild(newComment);
 
         // Clear the input fields
-        document.getElementById('name').value = '';
-        document.getElementById('comment').value = '';
+        clearInputFields(nameInput, commentInput);
     } else {
         alert('Please enter both your name and a comment.');
     }
+}
+
+function createCommentElement(name, comment) {
+    var commentContainer = document.createElement('div');
+    commentContainer.classList.add('comment');
+
+    var commentContent = document.createElement('p');
+    commentContent.innerHTML = '<strong>' + name + ':</strong> ' + comment;
+
+    commentContainer.appendChild(commentContent);
+
+    return commentContainer;
+}
+
+function clearInputFields(...inputs) {
+    inputs.forEach(input => (input.value = ''));
 }
