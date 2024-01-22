f7ec5482d262ada5568844e46e8997b183a6b5fa
GooseBooze <137728081+GooseBooze@users.noreply.github.com>
2024-01-22 18:06:27 +0100

    ss

--- thread.js
+++ thread.js
@@ -6,10 +6,20 @@ function submitComment() {
     var comment = commentInput.value.trim();
 
     if (name && comment) {
-        var commentSection = document.getElementById('comments');
+        // Create a unique identifier for the comment
+        var commentId = 'comment_' + Date.now();
 
-        var newComment = createCommentElement(name, comment);
-        commentSection.appendChild(newComment);
+        // Create a comment object with name and comment text
+        var commentObject = {
+            name: name,
+            comment: comment
+        };
+
+        // Save the comment to a cookie
+        document.cookie = commentId + '=' + JSON.stringify(commentObject) + '; expires=' + getCookieExpiration();
+
+        // Add the comment to the comments section
+        displayComment(commentObject);
 
         // Clear the input fields
         clearInputFields(nameInput, commentInput);
@@ -17,6 +27,39 @@ function submitComment() {
         alert('Please enter both your name and a comment.');
     }
 }
+
+function displayComment(comment) {
+    var commentSection = document.getElementById('comments');
+
+    var newComment = document.createElement('div');
+    newComment.classList.add('comment');
+    newComment.innerHTML = '<strong>' + comment.name + ':</strong> ' + comment.comment;
+
+    commentSection.appendChild(newComment);
+}
+
+function clearInputFields(...inputs) {
+    inputs.forEach(input => (input.value = ''));
+}
+
+function getCookieExpiration() {
+    var date = new Date();
+    date.setTime(date.getTime() + (7 * 24 * 60 * 60 * 1000)); // Set expiration to 7 days
+    return date.toUTCString();
+}
+
+// Load existing comments from cookies when the page loads
+window.onload = function () {
+    var allCookies = document.cookie.split('; ');
+
+    for (var i = 0; i < allCookies.length; i++) {
+        var cookie = allCookies[i].split('=');
+        if (cookie[0].startsWith('comment_')) {
+            var commentObject = JSON.parse(cookie[1]);
+            displayComment(commentObject);
+        }
+    }
+};
 
 function createCommentElement(name, comment) {
     var commentContainer = document.createElement('div');
